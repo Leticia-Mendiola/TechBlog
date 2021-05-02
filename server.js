853f59e38400526bfce38c5f3b9d081348fc2f91
Leticia-Mendiola <lmendiola509@gmail.com>
2021-05-02 18:11:25 -0500

    cleaned up server.js (and added .env file)

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
 const sequelize = require('./config/connection');
-
-// Create a new sequelize store using the express-session package
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -15,7 +12,6 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
-// Configure and link a session object with the sequelize store
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -26,7 +22,6 @@ const sess = {
   })
 };
 
-// Add express-session and store as Express.js middleware
 app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
