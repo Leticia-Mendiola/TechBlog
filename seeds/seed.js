b53ae03cef82b9e84c61caa3826a61233ca4e54a
Leticia-Mendiola <lmendiola509@gmail.com>
2021-05-03 15:38:41 -0500

    forgot to add exampleposts and examplecomments to seeds.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,22 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { Comments, Posts, User } = require('../models');
 
 const userData = require('./userData.json');
+const posts = require('./exampleposts.json');
+const comments = require('./examplecomments.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  await Posts.bulkCreate(posts, {
+    individualHooks: true,
+    returning: true,
+  });
+  await Comments.bulkCreate(comments, {
     individualHooks: true,
     returning: true,
   });
