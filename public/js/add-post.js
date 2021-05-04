83e04ef072963e874c30e22efbc87ad1169463e5
Leticia-Mendiola <lmendiola509@gmail.com>
2021-05-04 00:47:09 -0500

    widespread changes to add-post.js to post new posts

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -1,28 +1,24 @@
 async function newFormHandler(event) {
   event.preventDefault();
-  const dish_name = document.querySelector('#dish_name').value;
-  const description = document.querySelector('#description').value;
-  const guest_name = document.querySelector('#guest_name').value;
-  // The following is a ternary operator. It checks to see if has_nuts is checked. If it is, it will return true, otherwise, it will return false.
-  const has_nuts = document.querySelector('#has_nuts:checked') ? true : false;
-  // Send fetch request to add a new dish
-  const response = await fetch(`/api/dish`, {
+  const post_title = document.querySelector('#post_title').value;
+  const post_body = document.querySelector('#post_body').value;
+  const username = req.session.username;
+
+  const response = await fetch(`/`, {
     method: 'POST',
     body: JSON.stringify({
-      dish_name,
-      description,
-      guest_name,
-      has_nuts,
+      post_title,
+      username,
+      post_body,
     }),
     headers: {
       'Content-Type': 'application/json',
     },
   });
-  //if the dish is added, the 'all' template will be rerendered
   if (response.ok) {
     document.location.replace('/');
   } else {
-    alert('Failed to add dish');
+    alert('Failed to add post');
   }
 }
 
